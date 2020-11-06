b0e9787122cc2d29a5343bb9ca047bbfb3182593
gaofan <flys.gaofan@163.com>
2020-11-06 14:47:24 +0800

    ref:新增任务调度示例，配置。

--- src/main/java/com/seckill/agent/job/TestStatusJob.java
+++ src/main/java/com/seckill/agent/job/TestStatusJob.java
@@ -6,15 +6,19 @@ import com.xxl.job.core.log.XxlJobLogger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Slf4j
 @Component
 public class TestStatusJob {
 
     @SuppressWarnings("unused")
     @XxlJob("testPrint")
-    public ReturnT<String> testPrint() {
+    public ReturnT<String> testPrint(String param) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         XxlJobLogger.log("XXL-JOB, 引入打印测试.");
-        System.out.println("log = " + "打印测试！！！");
+        System.out.println("log = " + dateFormat.format(new Date())  +"打印测试！！！");
         return ReturnT.SUCCESS;
     }
 }
